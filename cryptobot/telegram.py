6604b0cac4badd20cc108479dc6e3d51328d345f
Aaron <38539400+Afitzy98@users.noreply.github.com>
2020-10-22 17:56:40 +0100

    Development (#12)
    * ability to post messages in telegram chat
    
    * fixed improt issues
    
    * printing url and response for debugging
    
    Co-authored-by: Aaron Fitzpatrick <a.fitzy98@gmail.com>

--- cryptobot/telegram.py
+++ cryptobot/telegram.py
@@ -17,9 +17,10 @@ def setWebhook():
 def sendMessage(message: str):
     reqUrl = BASE_URL + TG_BOT_TOKEN + "/sendMessage"
     params = f"?chat_id={TG_USER_ID}&message={message}"
-
+    print(reqUrl + params)
     try:
-        requests.post(reqUrl + params)
+        res = requests.post(reqUrl + params)
+        print(json.loads(res.content))
     except:
         print("ERROR.FAILED_TO_SEND_MESSAGE")
 
