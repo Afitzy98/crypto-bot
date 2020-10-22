ae8141c3d106171c92319f89b0a2cae9379a1032
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2020-10-22 17:40:29 +0100

    ability to post messages in telegram chat

--- cryptobot/telegram.py
+++ cryptobot/telegram.py
@@ -1,17 +1,28 @@
-import requests
 import json
+import requests
+from settings import TG_BOT_TOKEN, WEBHOOK_URL, TG_USER_ID
 
-class Telegram:
-    def __init__(self, botToken: str):
-        self.botToken = botToken
-        self.reqUrl = "https://api.telegram.org/bot"
+BASE_URL = "https://api.telegram.org/bot"
 
-    def setWebhook(self, webhookUrl: str):
-        reqUrl = self.reqUrl + self.botToken + "/setWebhook"
-        setUrl = webhookUrl + f"/{self.botToken}"
-        try:
-            requests.post(f"{reqUrl}?url={setUrl}")
-        except:
-            print("ERROR.FAILED_TO_SET_WEBHOOK")
+def setWebhook():
+    reqUrl = BASE_URL + TG_BOT_TOKEN + "/setWebhook"
+    setUrl = WEBHOOK_URL + f"/{TG_BOT_TOKEN}"
+
+    try:
+        requests.post(f"{reqUrl}?url={setUrl}")
+    except:
+        print("ERROR.FAILED_TO_SET_WEBHOOK")
+
+
+def sendMessage(message: str):
+    reqUrl = BASE_URL + TG_BOT_TOKEN + "/sendMessage"
+    params = f"?chat_id={TG_USER_ID}&message={message}"
+
+    try:
+        requests.post(reqUrl + params)
+    except:
+        print("ERROR.FAILED_TO_SEND_MESSAGE")
+
+
 
 
