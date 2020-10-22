470104e54e8c94aa4f5dce13cd94f7f27b68be5d
Aaron <38539400+Afitzy98@users.noreply.github.com>
2020-10-22 17:41:30 +0100

    ability to post messages in telegram chat (#10)
    Co-authored-by: Aaron Fitzpatrick <a.fitzy98@gmail.com>

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-from cryptobot.telegram import Telegram
-from cryptobot.utils import handleMessage
+from cryptobot.telegram import setWebhook
+from cryptobot.utils import handleRequest
 from flask import Flask, request, jsonify
-from settings import APP_SETTINGS, TG_BOT_TOKEN, WEBHOOK_URL, TG_USER_ID
+from settings import APP_SETTINGS
 
 
 app = Flask(__name__)
 app.config.from_object(APP_SETTINGS)
 
-tg = Telegram(TG_BOT_TOKEN)
-tg.setWebhook(WEBHOOK_URL)
+setWebhook()
 
 @app.route('/{}'.format(TG_BOT_TOKEN), methods=['POST'])
-def handleRequest():
-    
-    req = request.get_json()
-    message = req["message"]
-
-    if message and message["from"]["id"] == TG_USER_ID and message["text"]:
-        handleMessage(message["text"])
-
-    return 'ok'
+def webhookEndpoint():
+    try:
+        req = request.get_json()
+        handleRequest(req)
+    except:
+        print("ERROR.WHILE_HANDLING_REQUEST")
 
 if __name__ == '__main__':
     app.run(threaded=True, ssl_context=('cert.pem', 'key.pem'), port=80)
--- cryptobot/utils.py
+++ cryptobot/utils.py
@@ -1,2 +1,14 @@
+from settings import TG_USER_ID
+from telegram import sendMessage
+
 def handleMessage(message: str):
-    print(f"Message received: {message}")
+    # reply in chat with same message
+    sendMessage(message)
+
+def handleRequest(req: dict):
+    message = req["message"]
+
+    if message and message["from"]["id"] == TG_USER_ID and message["text"]:
+        handleMessage(message["text"])
+
+    return 'ok'
