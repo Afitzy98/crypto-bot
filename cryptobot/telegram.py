4e09791e403a0655b750ec1a20380cb4c0d21f0e
Aaron <38539400+Afitzy98@users.noreply.github.com>
2020-10-22 18:11:38 +0100

    Abiltiy to post message in chat complete (#15)
    * added ssl cert for https endpoint for webooks, added bot token env var
    
    * setting webhook url and printing incoming webhook requests
    
    * added gunicorn to requirements
    
    * parsing message from response and passing to handle message function
    
    * fixed spelling error, reorded imports
    
    * correctly parsing message from request
    
    * printing responses to see errors
    
    * parsing int from user id
    
    * passing correct string to handleMessage func
    
    * ability to post messages in telegram chat
    
    * ability to post messages in telegram chat (#10)
    
    Co-authored-by: Aaron Fitzpatrick <a.fitzy98@gmail.com>
    
    * fixed improt issues
    
    * Development (#11)
    
    * ability to post messages in telegram chat
    
    * fixed improt issues
    
    Co-authored-by: Aaron Fitzpatrick <a.fitzy98@gmail.com>
    
    * printing url and response for debugging
    
    * Development (#12)
    
    * ability to post messages in telegram chat
    
    * fixed improt issues
    
    * printing url and response for debugging
    
    Co-authored-by: Aaron Fitzpatrick <a.fitzy98@gmail.com>
    
    * sending correct params
    
    * returning ok afer request
    
    Co-authored-by: Aaron Fitzpatrick <a.fitzy98@gmail.com>

--- cryptobot/telegram.py
+++ cryptobot/telegram.py
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
+import json
+import requests
+from settings import TG_BOT_TOKEN, WEBHOOK_URL, TG_USER_ID
+
+BASE_URL = "https://api.telegram.org/bot"
+
+def setWebhook():
+    reqUrl = BASE_URL + TG_BOT_TOKEN + "/setWebhook"
+    setUrl = WEBHOOK_URL + f"/{TG_BOT_TOKEN}"
+
+    try:
+        requests.post(f"{reqUrl}?url={setUrl}")
+    except:
+        print("ERROR.FAILED_TO_SET_WEBHOOK")
+
+
+def sendMessage(message: str):
+    reqUrl = BASE_URL + TG_BOT_TOKEN + "/sendMessage"
+    params = f"?chat_id={TG_USER_ID}&text={message}"
+    try:
+        res = requests.post(reqUrl + params)
+    except:
+        print("ERROR.FAILED_TO_SEND_MESSAGE")
+
+
+
+
+=======
 import requests
 import json
 
@@ -15,3 +44,4 @@ class Telegram:
             print("ERROR.FAILED_TO_SET_WEBHOOK")
 
 
+>>>>>>> 6e814306ae6d7ca036bc72a3668e210b98842350
