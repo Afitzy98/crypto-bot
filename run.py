f2b1b86b55b60cb7b7efb7f38f926140e8304a5a
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2020-11-09 22:23:29 +0000

    removed vars for shutting down scheduler

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-import signal
-
 from cryptobot import app
-from cryptobot.scheduler import start_scheduler, shutdown_scheduler
+from cryptobot.scheduler import start_scheduler
 
 if __name__ == "__main__":
     app.run(threaded=True, port=3001)
     start_scheduler()
-    signal.signal(signal.SIGTERM, lambda signum, frame: shutdown_scheduler())
