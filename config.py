fcf9996ce0911799089ebce22742198645e4850c
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2020-11-10 18:48:40 +0000

    using flask apscheduler

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import os
-from settings import APP_SECRET_KEY
+
+from settings import APP_SECRET_KEY, DB_URI
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,6 +13,22 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = APP_SECRET_KEY
+    SCHEDULER_JOBSTORES = {
+        "default": SQLAlchemyJobStore(url=DB_URI),
+    }
+    SCHEDULER_EXECUTORS = {
+        "default": ThreadPoolExecutor(20),
+        "processpool": ProcessPoolExecutor(5),
+    }
+    SCHEDULER_JOB_DEFAULTS = {
+        "coalesce": False,
+        "max_instances": 3,
+        "misfire_grace_time": 3 * 60,
+    }
+    SCHEDULER_TIMEZONE = "UTC"
+
+    SCHEDULER_API_ENABLED = True
+
 
 class ProductionConfig(Config):
     DEBUG = False
--- cryptobot/__init__.py
+++ cryptobot/__init__.py
@@ -1,8 +1,9 @@
 import atexit
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_apscheduler import APScheduler
 from settings import APP_SETTINGS, DB_URI
+
 
 app = Flask(__name__)
 app.config.from_object(APP_SETTINGS)
@@ -10,12 +11,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+sched = APScheduler()
+sched.init_app(app)
+sched.start()
+atexit.register(lambda: sched.shutdown())
+
 from cryptobot import routes
 from cryptobot.telegram import set_webhook, send_message
-from cryptobot.scheduler import scheduler
 
-scheduler.start()
 send_message("🔄 Scheduler has restarted")
 set_webhook()
-
-atexit.register(lambda: scheduler.shutdown())
