using simple moving average crossover strategy with fixed portfolio & ratios

cryptobot/binance.py
@@ -10,10 +10,9 @@ from binance.client import Client
 from binance.enums import *
 from cryptobot import app
 
-from .constants import AUTO_REPAY, MARGIN_BUY, NO_SIDE_EFFECT, NUM_SYMBOLS
+from .constants import AUTO_REPAY, MARGIN_BUY, NO_SIDE_EFFECT, SYMBOLS
 from .enums import Position
 from .model import add_position, get_position
-from .scheduler import get_symbols
 from .telegram import send_message
 
 client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
@@ -65,7 +64,7 @@ def handle_decision(position: Position, symbol: str):
 
     else:
         handle_exit_positions(symbol, prevPosition)
-
+        
     add_position(get_current_ts_dt(), symbol, position)
 
 
@@ -91,7 +90,7 @@ def get_all_valid_symbols():
 
 def get_useable_usdt_qty(symbol: str):
     usdt_in_coins = 0
-    symbols = [s for s in get_symbols() if s != symbol]
+    symbols = [s for s in SYMBOLS if s != symbol]
     for s in symbols:
         bal = np.float(client.get_asset_balance(asset=s[:-4])["free"])
         ticker = np.float(get_ticker(s)["bidPrice"])
@@ -100,7 +99,7 @@ def get_useable_usdt_qty(symbol: str):
     free_usdt = np.float(client.get_asset_balance("USDT")["free"])
    
 
-    useable_usdt = (free_usdt + usdt_in_coins) * (1/NUM_SYMBOLS)
+    useable_usdt = (free_usdt + usdt_in_coins) * (1/len(SYMBOLS))
 
     return free_usdt if useable_usdt > free_usdt else useable_usdt
 

tests/strategy.py
@@ -8,12 +8,11 @@ from cryptobot import app
 from cryptobot.binance import get_data
 from cryptobot.enums import Position
 from cryptobot.model import HourlyPosition
-from cryptobot.strategy import apply_strategy_on_history  # , apply_strategy
-from cryptobot.strategy import task
+from cryptobot.strategy import apply_strategy, task
 
 NO_SIDE_RET_VAL = np.zeros((150, 5))
 LONG_RET_VAL = np.linspace((1, 1, 1, 1, 1), (150, 150, 150, 150, 150), 150)
-HOURLY_POS = HourlyPosition(time=datetime.now(), symbol="TEST", position=Position.LONG)
+HOURLY_POS = HourlyPosition(time=datetime.now(), symbol="TEST", position=Position.NONE)
 
 DF = pd.DataFrame(LONG_RET_VAL, columns=["Open", "High", "Low", "Close", "Volume"])
 
@@ -22,22 +21,38 @@ class TestStrategy(unittest.TestCase):
 
     @mock.patch("cryptobot.db.session")
     @mock.patch("cryptobot.model.HourlyPosition")
+    @mock.patch(
+        "binance.client.Client.get_historical_klines", side_effect=Exception
+    )
+    def test_task_failing(
+        self, mock_get_data, mock_get_pos, mock_db_session, mock_req_post
+    ):
+        with app.app_context():
+            task()
+            mock_get_pos.assert_not_called()
+            mock_get_data.assert_called()
+            mock_req_post.assert_called()
+
+
+    @mock.patch("cryptobot.db.session")
+    @mock.patch("cryptobot.model.HourlyPosition")
+    @mock.patch(
+        "binance.client.Client.get_historical_klines", return_value=NO_SIDE_RET_VAL
+    )
     def test_strategy_no_side(
-        self, mock_get_pos, mock_db_session, mock_req_post
+        self, mock_get_data, mock_get_pos, mock_db_session, mock_req_post
     ):
         mock_get_pos.query.get.return_value = HOURLY_POS
         with app.app_context():
-            task(["a", "b"])
+            task()
+            mock_get_pos.assert_called()
+            mock_get_data.assert_called()
             mock_req_post.assert_not_called()
 
-
-
-    def test_apply_strategy_on_history(self, mock_req_post):
-        res = apply_strategy_on_history(DF, "SYM")
-        self.assertEqual(res, {
-            "ret": 0.10294117647058823,
-            'symbol': 'SYM'
-        })
+    def test_strategy_long(self, mock_req_post):
+        pos = apply_strategy(DF)
+        mock_req_post.assert_not_called()
+        self.assertEqual(pos, Position.LONG)
 
 if __name__ == "__main__":
     unittest.main()