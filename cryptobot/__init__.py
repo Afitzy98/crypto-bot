03aacc582080cf87e6fe4db9ba68dbd284d39d64
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2021-02-18 17:16:47 +0000

    creating db on startup

--- cryptobot/__init__.py
+++ cryptobot/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 from settings import APP_SETTINGS, DB_URI
-
 
 app = Flask(__name__)
 app.config.from_object(APP_SETTINGS)
@@ -10,9 +8,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+db.create_all()
+db.session.commit()
+
 from . import routes  # Import routes
 from .scheduler import sched
-from .telegram import set_webhook, send_message
+from .telegram import send_message, set_webhook
 
 sched.start()
 send_message("🔄 Scheduler has restarted")
