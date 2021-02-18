2775c8545995023ae9019d3ea4acde9c4fb4ecf8
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2021-02-18 17:31:53 +0000

    fixed error in test case

--- tests/model.py
+++ tests/model.py
@@ -1,13 +1,12 @@
-from datetime import datetime
-import numpy as np
 import unittest
+from datetime import datetime
 from unittest import mock
 
+import numpy as np
 from cryptobot import app
-
 from cryptobot.binance import get_data
 from cryptobot.enums import Position
-from cryptobot.model import HourlyPosition, get_position, add_position
+from cryptobot.model import HourlyPosition, add_position, get_position
 
 HOURLY_POS = HourlyPosition(time=datetime.now(), symbol="TEST", position=Position.NONE)
 
@@ -29,7 +28,9 @@ class TestModel(unittest.TestCase):
 
     @mock.patch("cryptobot.db.session.add", side_effect=Exception)
     @mock.patch("cryptobot.db.session.commit")
-    def test_add_pos_failing(self, mock_db_commit, mock_add):
+    @mock.patch("cryptobot.model.HourlyPosition")
+    def test_add_pos_failing(self, mock_get_pos, mock_db_commit, mock_add):
+        mock_get_pos.query.get.return_value = None
         with app.app_context():
             add_position(datetime.now(), "", Position.NONE)
             mock_add.assert_called_once()
@@ -37,11 +38,23 @@ class TestModel(unittest.TestCase):
 
     @mock.patch("cryptobot.db.session.add")
     @mock.patch("cryptobot.db.session.commit")
-    def test_add_pos(self, mock_db_commit, mock_add):
+    @mock.patch("cryptobot.model.HourlyPosition")
+    def test_add_pos(self, mock_get_pos, mock_db_commit, mock_add):
+        mock_get_pos.query.get.return_value = None
         with app.app_context():
             add_position(datetime.now(), "", Position.NONE)
             mock_add.assert_called_once()
             mock_db_commit.assert_called_once()
+
+    @mock.patch("cryptobot.db.session.add")
+    @mock.patch("cryptobot.db.session.commit")
+    @mock.patch("cryptobot.model.HourlyPosition")
+    def test_add_pos_already_entered(self, mock_get_pos, mock_db_commit, mock_add):
+        mock_get_pos.query.get.return_value = HOURLY_POS
+        with app.app_context():
+            add_position(datetime.now(), "", Position.NONE)
+            mock_add.assert_not_called()
+            mock_db_commit.assert_not_called()
 
 
 if __name__ == "__main__":
