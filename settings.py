8c7446b3bcae5db1d707e706637d3e95b193ab8b
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2021-03-02 13:47:39 +0000

    removed as env var for google credentials

--- settings.py
+++ settings.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 import os
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ".\DevCryptoBot-811d9b05d5d8.json"
+
 APP_SETTINGS = os.getenv("APP_SETTINGS")
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
 BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
