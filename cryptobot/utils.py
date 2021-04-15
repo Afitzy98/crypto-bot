d4b4c23fba2b844e555136c5daf62789c0305e7d
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2021-04-15 11:22:05 +0100

    ùpdated timestamp util functions

--- cryptobot/utils.py
+++ cryptobot/utils.py
@@ -2,13 +2,17 @@ from datetime import datetime, timedelta
 
 
 def get_current_ts_dt():
-    return datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+    now = datetime.now()
+    hr = now.hour - (now.hour % 4)
+    return now.replace(microsecond=0, second=0, minute=0, hour=hr)
 
 
 def get_previous_ts_dt():
+    now = datetime.now()
+    hr = now.hour - (now.hour % 4)
     return datetime.now().replace(
-        microsecond=0, second=0, minute=0, hour=0
-    ) - timedelta(days=1)
+        microsecond=0, second=0, minute=0, hour=hr
+    ) - timedelta(hours=4)
 
 
 def get_nearest_hour_dt():
